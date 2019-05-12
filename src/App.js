d98dc08ed91766c9ebd3e4052eca2d51a299204d
VictoriaDejneka <avyvayv@gmail.com>
2019-05-12 19:41:19 +0300

    add initial header and axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './header/Header.js';
 
 function App() {
   return (
     <div className="App">
+      <Header text={"text"} abc={""}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
